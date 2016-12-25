9913e30298371a7706de1a9517246d738235af6b
ryans <ryans@lucidyne.com>
2016-12-24 16:01:18 -0800

    Added little script to help with the jack connection details

---
+++ tools/connect.sh
@@ -0,0 +1,5 @@
+jack_connect libol:out_x simulator:in_x
+jack_connect libol:out_y simulator:in_y
+jack_connect libol:out_g simulator:in_g
+jack_connect libol:out_r simulator:in_r
+jack_connect libol:out_b simulator:in_b
